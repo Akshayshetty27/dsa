f7fbeb8aa92d49396682a344be134593e7a06863
Akshay07 <shettyakshay333@gmail.com>
2024-10-16 21:08:45 +0530

    oops

--- oopsconcept lecture1/Main.java
+++ oopsconcept lecture1/Main.java
@@ -6,23 +6,37 @@ class Main
     public static void main(String args[])
     {
 // declaring an object s1 of student class and initializing 
-        Student s1= new Student( 5, "akshay" ,45);
-        System.out.println(s1.name +" "+s1.roll +" "+s1.mark1);
+        Student s1= new Student( 4, "akshay  " ,45);
+        System.out.println(s1.name +" "+s1.roll +" "+s1.age);
         Student s2= new Student( 1, "abhilash" ,45);
-        System.out.println(s2.name +" "+s2.roll +" "+s2.mark1);
+        System.out.println(s2.name +" "+s2.roll +" "+s2.age);
+        // java can have multiple constructor  i.e it can be override 
+      // default constructor ( without any parameters )
+        Student s3 = new Student();
+        System.out.println(" "+s3.roll +" "+s3.name +" "+ s3.age);
+        // this keyword acts as a  refference of an object creted of a class 
            }
 
     static  class Student 
     {
       int roll;
       String name;
-      int mark1,mark2;
+      int age;
+      // parameterized constructors 
       Student(int roll ,String name ,int  mark1)
       {
         this.roll= roll;
         this.name = name;
-        this.mark1= mark1;
+        this.age= mark1;
         // System.out.println(" roll number  "+roll+" , name "+ name + " and  marks :"+mark1+" ,"+mark2);
+      }
+      Student()
+      {
+        // System.out.println(" hello");
+        this.roll= 13;
+        this.name="akhsay";
+        this.age=21;
+        
       }
     }
 }
