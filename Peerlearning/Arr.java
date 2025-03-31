c304782ccb3c9c5d55cca89180db8cea419f317e
akshay <shettyakshay333@gmail.com>
2025-04-01 00:05:16 +0530

    practice

--- Peerlearning/Arr.java
+++ Peerlearning/Arr.java
@@ -6,11 +6,13 @@ class Arr
         int arr[] = { 4,5,5,1,2,3,4};
         Arrays.sort(arr);
 
-        for( int i=1; i< arr.length ; i++)
+for( int i : arr)
+System.out.println(i);
+        for( int i=arr.length  - 2; i>1 ; i--)
         {
-            if(arr[i] < arr[i - 1] )
+            if(arr[ i - 1 ] < arr[ i ] )
             {
-                System.out.println(" "+arr[i]);
+                System.out.println("second highest : "+arr[i-  1 ]);
                 System.exit(0);
             }
         }
