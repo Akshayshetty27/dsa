6db955afece1cb79d8c99bd017a826c0989d9eae
akshay <shettyakshay333@gmail.com>
2025-04-01 22:30:07 +0530

    Longest_substringLength

---
+++ restart_/Long.java
@@ -0,0 +1,33 @@
+// Find the Longest Substring Without Repeating Characters – Given a string, find the longest substring without repeating characters.
+import java.util.HashSet;
+
+class Long 
+{
+
+    public static void main (String args[])
+
+{
+
+    String str= "abcdeabbsabcde";
+    // String long=""
+    int max=0;
+    HashSet<Character> set= new HashSet<>();
+    int left=0;
+    StringBuilder sb= new StringBuilder();
+    for( int right=0 ; right< str.length() ; right++)
+    {
+        while ( set.contains( str.charAt(right)))
+        {
+            set.remove(str.charAt(left));
+            left++;
+        }
+      
+        set.add(str.charAt(right));
+        max= Math.max( max , right - left +1);
+
+    }    
+
+
+System.out.println(max+ " ");
+}
+}
