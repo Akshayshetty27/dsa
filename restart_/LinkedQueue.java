5edbdf5696b8328ea2758892527aa9e327ccb2b5
akshay <shettyakshay333@gmail.com>
2025-04-06 22:25:36 +0530

    FullLinkedueue

--- restart_/LinkedQueue.java
+++ restart_/LinkedQueue.java
@@ -13,7 +13,7 @@ class LinkedQueue
 
     static Node head = null;
 
-    public static  void enQueue(int data)
+    public   void enQueue(int data)
     {
         Node newNode = new Node(data);
         if( head == null)
@@ -24,28 +24,62 @@ class LinkedQueue
 
         Node temp = head;
 
-        while( temp != null )
-        {
-            temp = temp.next;
-
-        }
-        temp=  newNode;
-
+while (temp.next != null) {
+    temp = temp.next;
+}
+temp.next = newNode;
 
     }
 
-    public static int  deQueue()
+    public int  deQueue()
     {
         if( head == null)
         {
             System.out.println(" the queue is empty ");
-            return ;
-        
-
+            return  0;   
     }
+
+    int data= head.data;
+    System.out.println(" the deleted item :"+data);
+
+    head= head.next;
+
+    return data;
+    }
+
+    public void display()
+    {
+        if( head == null)
+        {
+            System.out.println(" the queue is Empty ");
+            return ;
+        }
+    Node temp = head;
+    while( temp != null)
+    {
+        System.out.println(" "+ temp.data);
+        temp= temp.next;
+    }
+    }
+
+public  void top( )
+{
+    if(head == null )
+    {
+        System.out.println(" the  queue is Empty ");
+        return ;
+    }
+    System.out.println(" "+ head.data);
+}
     public static void  main( String args[])
     {
-
-
+        LinkedQueue node = new LinkedQueue();
+        node.enQueue(1);
+        node.enQueue(2);
+        node.enQueue(3);
+        node.display();
+        node.deQueue();
+        node.top();
+    
     }
 }
