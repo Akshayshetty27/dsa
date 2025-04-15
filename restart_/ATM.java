b5004a8f2230a6c5fe1ca76e6442feb9953bfe76
akshay <shettyakshay333@gmail.com>
2025-04-15 22:43:29 +0530

    updated_Atm

--- restart_/ATM.java
+++ restart_/ATM.java
@@ -3,19 +3,16 @@ import java.util.Random;
 
 class ATM
 {
-            static int total_amount = 500;
-
+    static int total_amount = 500;
     public static void main(String args[])
     {
         int choice;
         Scanner sc= new Scanner(System.in);
         boolean ch = true;
-
         while(ch)
         {
             System.out.println(" Enter your choice \n");
             choice= sc.nextInt();
-
             switch(choice)
             {
                 case 1: 
@@ -30,41 +27,32 @@ class ATM
                     System.out.println(" take out your "+withdraw_amount+ " rs \n available amount is : "+total_amount);
                 }
                  break;
-
                  case 2:
                     System.out.println(" enter the amount you want to add ");
                     int add_amount = sc.nextInt();
 
                     total_amount += add_amount;
                     System.out.println(" "+add_amount+" has been added to your account "+ " total amount available = "+ total_amount);
-
                     break;
-
                 case 3: 
                 System.out.println(" available balance : "+total_amount);
                 break;
-
                 case 4: 
                 System.out.println(" enter the code you seeing in the terminal \n");
                 Random random = new Random();
                 int rand = random.nextInt(9000)+ 1000;
-
                 System.out.println(" "+rand);
-
                 int userinput= sc.nextInt();
                 if( userinput == rand)
                 {
                     System.out.println(" your ATM has been blocked ");
                     System.exit(0);
-
                 }
                 break;
-
                 default :
                 System.out.println(" enter valid choice ");
                 break;
             }
-
             System.out.println(" you want to continue ");
             ch= sc.nextBoolean();
 
@@ -76,4 +64,4 @@ class ATM
 }
 
 
-// want to add  1. withdraw  2. add cash  3. check amount  4. Block  
+// want to add method ->      1. withdraw  2. add cash  3. check amount  4. Block  
