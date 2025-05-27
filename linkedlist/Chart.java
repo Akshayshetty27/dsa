0312d52a38387b9afa26ca2ad762158b9c974960
akshay <shettyakshay333@gmail.com>
2025-05-27 21:58:21 +0530

    practice

--- linkedlist/Chart.java
+++ linkedlist/Chart.java
@@ -18,7 +18,7 @@ class Chart {
     public static boolean isPrime(int n) {
         if (n <= 1) return false;
         if (n == 2) return true;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        for (int i = 2; i <=  n / 2 ; i++) {
             if (n % i == 0) return false;
         }
         return true;
