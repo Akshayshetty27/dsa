71b74ee732cfbb8cc85c1ec63ef1d3477f51a2b7
akshay <shettyakshay333@gmail.com>
2025-07-30 22:02:16 +0530

    Fifth_Question

---
+++ leetcode/Fifth.java
@@ -0,0 +1,21 @@
+class Fifth
+{
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;  
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];  
+                k++;
+            }
+        }
+                return k;
+    }
+    public static void main(String[] args)
+    {
+       int arr[] = {3, 2, 2, 3};
+       int val = 3;
+       int newLength = removeElement(arr, val);
+         System.out.println("New length of the array: " + newLength);
+    }
+}
+
