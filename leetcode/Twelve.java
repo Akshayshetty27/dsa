c475f4c55adb1e1226e2ff7107de43b42fbf4f99
akshay <shettyakshay333@gmail.com>
2025-09-15 21:39:46 +0530

    Merge two list

---
+++ leetcode/Twelve.java
@@ -0,0 +1,53 @@
+class Twelve {
+    public static class ListNode {  
+        int val;
+        ListNode next;
+
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (list1 != null) tail.next = list1;
+        else tail.next = list2;
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        Twelve solution = new Twelve();
+        ListNode mergedList = solution.mergeTwoLists(list1, list2);
+
+        ListNode current = mergedList;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+}
