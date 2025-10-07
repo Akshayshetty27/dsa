78a20133fea8cbbe8b73fcf9a35a335f3563017f
akshay <shettyakshay333@gmail.com>
2025-10-07 22:20:43 +0530

    String inbuilt methods in pythons

---
+++ python for Data enginner/Three.py
@@ -0,0 +1,24 @@
+# String Methods in Python
+# String methods are built-in functions that can be used to manipulate and work with strings in Python
+str = "Hello , Welcome to ILP";
+
+print(str.upper())
+print(str.lower())
+print(str.find("ILP"))
+print(str.rindex("l"))
+print(str.capitalize())
+print(str.strip())
+print(str.lstrip())
+print(str.count("l"))
+print(str.rstrip())
+print(str.replace("ILP", "Python"))
+print(str.split(","))
+
+print(str.index("ILP"))
+
+
+print(str.startswith("Hello"))
+print(str.endswith("ILP"))
+print(len(str))
+print(str.isalpha())
+print(str.isdigit())
